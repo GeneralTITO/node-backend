84c9cbfd5708870f7dce25b60fa7abb626e8d812
tito <twiceqbit@gmail.com>
2025-03-04 19:18:55 -0300

    feat: initial config

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,8 @@
 import "express-async-errors";
 import express, { Application, json } from "express";
-import middlewares from "./middlewares";
-import { PrismaClient } from "@prisma/client";
-import {
-  categoryRouter,
-  ingredientRouter,
-  recipeRouter,
-  sessionRouter,
-  userRouter,
-} from "./routers";
 
 const app: Application = express();
-const prisma = new PrismaClient();
 
 app.use(json());
-
-// Defina suas rotas aqui
-app.use("/categories", categoryRouter);
-app.use("/ingredients", ingredientRouter);
-app.use("/recipes", recipeRouter);
-app.use("/sessions", sessionRouter);
-app.use("/users", userRouter);
-
-app.use(middlewares.handleError);
 
 export default app;
